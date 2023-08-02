30dd8bdc4b4b9716123dd2aacef433d4f5c80d30
NamasakaLennox <namasakalennox254@gmail.com>
2023-08-02 11:08:15 +0300

    correction for task 9

--- len_test/9-binary_tree_height.c
+++ len_test/9-binary_tree_height.c
@@ -1,6 +1,35 @@
 #include "binary_trees.h"
 
 /**
+ * recursive_height - gets the height in a recursive manner
+ * @tree: the tree to get height
+ *
+ * Return: returns the height of a tree
+ */
+size_t recursive_height(const binary_tree_t *tree)
+{
+	size_t height = 0, temp_height;
+
+	if (tree == NULL)
+		return (0);
+
+	height++;
+	temp_height = recursive_height(tree->left);
+	/* check max height items on left side */
+	height = height > temp_height
+		? height
+		: height + temp_height;
+
+	temp_height = recursive_height(tree->right);
+	/* check max height items on right side */
+	height = height > temp_height
+		? height
+		: height + temp_height;
+
+	return (height);
+}
+
+/**
  * binary_tree_height - height of the binary tree
  * @tree: the tree to get the height
  *
@@ -8,24 +37,12 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height = 0, temp_height;
+	size_t height = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL || tree->right != NULL)
-		height++;
-	temp_height = binary_tree_height(tree->left);
-	/* check max height items on left side */
-	height = height > temp_height
-		? height
-		: height + temp_height;
-
-	temp_height = binary_tree_height(tree->right);
-	/* check max height items on right side */
-	height = height > temp_height
-		? height
-		: height + temp_height;
+	height = recursive_height(tree) - 1;
 
 	return (height);
 }
